5efd20dc6be7d8a266a68ec635bde2f688664cef
Alexander Soto Cardona <asoto@innventto.com>
2016-04-20 20:22:28 -0500

    working stage

--- dev/js/api/API_URL.js
+++ dev/js/api/API_URL.js
@@ -1,7 +1,7 @@
 var env = process.env.NODE_ENV || 'dev';
 var END_POINTS = {
 	dev: "http://localhost:4000/api",
-	stage: "http://stage.nigma.com:4000/api",
+	stage: "http://www.nigma.api.grupo-guiame.org/api",
 	production: "http://nigma.com:4000/api"
 }
 
